c7efaafc693c6b74c1beee594fb36ecebe873eb7
Eliasdb <>
2021-04-17 15:40:13 +0000

    a

--- main.cpp
+++ main.cpp
@@ -2,27 +2,37 @@
 #include <string>
 #include <cstring>
 
-using namespace std;
-
 #define numeros 8
 #define posicoes 4
 
-char teclado[numeros][posicoes] = 
-{
-{'a', 'b', 'c', '/0'}, 
-{'d', 'e', 'f', '/0'},
-{'g', 'h', 'i', '/0'},
-{'j', 'k', 'l', '/0'},
-{'m', 'n', 'o', '/0'},
-{'p', 'q', 'r', 's'},
-{'t', 'u', 'v', '/0'},
-{'w', 'x', 'y', 'z'}
+using namespace std;
+
+char teclado[numeros][posicoes] = {
+	{'a', 'b', 'c', '\0'},
+	{'d', 'e', 'f', '\0'},
+	{'g', 'h', 'i', '\0'},
+	{'j', 'k', 'l', '\0'},
+	{'m', 'n', 'o', '\0'},
+	{'p', 'q', 'r', 's'},
+	{'t', 'u', 'v', '\0'},
+	{'w', 'x', 'y', 'z'}
 };
 
 
 
-
 int main() {
+ char palavra_P[50];
+ string palavra;
+
+ cout << "Digite a palavra desejada:" << endl;
+ cin >> palavra;
+
+if(palavra.length() <= 50)
+{
+  cout << "Você devera precionar as seguintes teclas e respectivamente o número de vezes para formar a palavra desejada:" << endl;
+  
+}
+
 
   
   
