d8e5de8cb8cd5d8643d0f6aa287e00679d549cf2
Eliasdb <>
2021-04-17 16:44:52 +0000

    Finalizado

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <cstring>
 
+using namespace std;
+
 #define numeros 8
 #define posicoes 4
-
-using namespace std;
 
 char teclado[numeros][posicoes] = {
 	{'a', 'b', 'c', '\0'},
@@ -18,33 +18,35 @@ char teclado[numeros][posicoes] = {
 	{'w', 'x', 'y', 'z'}
 };
 
-void leitura(char letra);
+void leitura(char letra); 
 
 int main() {
- char palavra_P[50];
- string palavra;
+	char palavra_P[50];
+	string palavra;
 
- cout << "Digite a palavra desejada:" << endl;
- cin >> palavra;
+  cout << "Digite a palavra desejada: " << endl;
+	cin >> palavra;
+	
+	if(palavra.length() <= 50){
+		strcpy(palavra_P, palavra.c_str());
 
-if(palavra.length() <= 50)
-{
-  cout << "Você devera precionar as seguintes teclas e respectivamente o número de vezes para formar a palavra desejada:" << endl;
-  
+    cout << "Você devera precionar as seguintes teclas e respectivamente o número de vezes para formar a palavra desejada:" << endl;
+		
+		for(int i = 0; palavra_P[i] != '\0'; i++){
+			leitura(palavra_P[i]);
+		}
+	} 
+	else{
+		cout << "Sua palavara precisa ter no máximo 50 caracteres" << endl;
+	}
 }
 
 void leitura(char letra){
-  for(int i=0; i < numeros; i++){
-    for(int j=0; j < posicoes; j++){
-      if(teclado[i][j] == letra)
-      {
-        cout << "#" << i + 1 << "=" << j + 1 << endl;
-      }
-    }
-  }
-}
-
-  
-  
-  
+	for(int i = 0; i <= numeros; i++){
+		for(int j = 0; j <= posicoes; j++){
+			if(teclado[i][j] == letra){
+				cout << "#" << i + 1 << "=" << j + 1 << endl;
+			}
+		}
+	}
 }
